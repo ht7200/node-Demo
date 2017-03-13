8af71d2feacf62524d9e2e2e7216211c815c2a8a
ht7200 <251741906@qq.com>
2017-03-13 17:45:30 +0800

    delete

--- config/wechat.cfg.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	grant_type: 'client_credential',
-	appid: 'wxf8337b6f556b0bb8',
-	secret: 'cd8fccf9052a3bccae25f3fa398fe44c',
-	noncestr:'Wm3WZYTPz0wzccnW',
-	accessTokenUrl:'https://api.weixin.qq.com/cgi-bin/token',
-	ticketUrl:'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
-	cache_duration:1000*60*60*24
-}
